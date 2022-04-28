dbba8f27f88cf28f1f8c97b10a2169b5c0b80d03
Fernando Lopez <fer68769@gmail.com>
2022-04-28 14:19:01 -0500

    Validación de campos tracks

--- Routes/tracks.js
+++ Routes/tracks.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getItems, getItem, createItem } = require('../Controllers/tracks');
+const { validatorCreateItem } = require('../validators/tracks');
 
 // http://localhost/tracks 
 router.get('/', getItems);
-router.post('/', createItem);
+router.post('/', validatorCreateItem, createItem);
 
 module.exports = router;
